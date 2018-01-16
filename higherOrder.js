f6da18b2ca28937caf814bf0117116a07915c36e
parth <srivastavaparth2016@gmail.com>
2018-01-16 10:15:39 +0530

    test case added to check counter and the number of times being recursed

--- higherOrder.js
+++ higherOrder.js
@@ -1,10 +1,17 @@
+let counter =0;
 var repeat = function repeat(operation,num)
 {
-  operation();
   if(num>0)
   {
+    counter+=1;
+    operation();
     repeat(operation,num-1);
   }
 }
 
+// var func = function(){
+//   console.log("callback function being executed",counter);
+// }
+// repeat(func,5);
+// console.log("checking the number of times recursion happens",counter );
 module.exports = repeat;
