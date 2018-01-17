db8d164557c21ed89274e280a6b1ff5cffd98aad
parth <srivastavaparth2016@gmail.com>
2018-01-17 12:49:50 +0530

    minor changes

--- reduce.js
+++ reduce.js
@@ -1,12 +1,13 @@
 function reduce(arr, fn, initial) {
   return function recursion(acc,index){
-    if(index>=arr.length){
+    if(index>=arr.length)
+    {
       return acc;
     }
     else {
-      return recursion(fn(acc,arr[index],index,initial),index+1);
+      return recursion(fn(acc,arr[index],index,arr),index+1);
     }
-  };
+  }(initial,0);
 }
 
 module.exports = reduce;
@@ -37,23 +38,23 @@ let ans1 = { lorem: 1,
   mollit: 1,
   sit: 1 };
 
-console.log('test case 1 for the use case ',JSON.stringify(reduce(test1,recur,{})({},0))=== JSON.stringify(ans1));
+console.log('test case 1 for the use case ',JSON.stringify(reduce(test1,recur,{}))=== JSON.stringify(ans1));
 
 let test2 = [];
 let ans2 = {};
 
-console.log('test case 2 for empty array', JSON.stringify(reduce(test2,recur,{})({},0))=== JSON.stringify(ans2));
+console.log('test case 2 for empty array', JSON.stringify(reduce(test2,recur,{}))=== JSON.stringify(ans2));
 
 
 let test3 = ['','','','','apple'];
 let ans3 = {'': 4, 'apple':1};
 
-console.log('test case 2 for empty strings', JSON.stringify(reduce(test3,recur,{})({},0))=== JSON.stringify(ans3));
+console.log('test case 2 for empty strings', JSON.stringify(reduce(test3,recur,{}))=== JSON.stringify(ans3));
 
 let test4 = ['CAPITAL','capital'];
 let ans4 = {'CAPITAL': 1,'capital': 1};
 
-console.log('test case 2 for different cases strings', JSON.stringify(reduce(test4,recur,{})({},0))=== JSON.stringify(ans4));
+console.log('test case 2 for different cases strings', JSON.stringify(reduce(test4,recur,{}))=== JSON.stringify(ans4));
 
 
 function recur(acc,head,index,initial)
