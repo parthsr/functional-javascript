19d9911cb95bda7118fd7cdb7baa6cef8d18df24
parth <srivastavaparth2016@gmail.com>
2018-01-18 00:07:33 +0530

    test cases added for log.js

--- log.js
+++ log.js
@@ -9,9 +9,8 @@ function logger(namespace) {
 module.exports = logger;
 
 
-// logger
-//
-// let a = {'0':'parth','1':'anmol'};
+
+// let a = {'0':'parth','1':'anmol','length':2};
 // // a=[1,2,3];
 // console.log(slice.call(a));
 //
@@ -22,3 +21,14 @@ module.exports = logger;
 // // a.slice(1);
 // //
 // // slice.call(a,1);
+
+logger('hello')('hi','how','are','you?');
+console.log('valid test case 1',true);
+logger('hello')();
+console.log('valid test case 2 with no extra params',true);
+logger()('hi','how','are','you');
+console.log('invalid test case 3 with no namespace and params existing',false);
+logger('')('hi','how','are','you');
+console.log('valid test case 4 with empty namespace and params existing',true);
+logger('')('');
+console.log('valid test case 5 with empty namespace and empty params', true);
