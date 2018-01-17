007ad7b1e2055ae2bb1a307b3f926a1e9b4af5d1
parth <srivastavaparth2016@gmail.com>
2018-01-17 13:42:57 +0530

    3rd test case for duck.js

--- duck.js
+++ duck.js
@@ -13,3 +13,4 @@ module.exports = duckCount;
 
 console.log('valid test case 1', duckCount({'quack':'true'}, {'hello':'false'})===1);
 console.log('test case 2 for empty args',duckCount()===0);
+console.log('test case for changing object ', duckCount({'quack': 'false'})===1);
