5225b26437536d35a90ae130e6a08b47ff5b6e15
parth <srivastavaparth2016@gmail.com>
2018-01-17 23:32:58 +0530

    log.js

--- log.js
+++ log.js
@@ -1,10 +1,24 @@
 let slice = Array.prototype.slice;
 
 function logger(namespace) {
-  // SOLUTION GOES HERE
 
   return function toApply(){
-
-  }.apply(namespace);
+    console.log(namespace , slice.apply(arguments).join(' '));
+  };
 }
 module.exports = logger;
+
+
+// logger
+//
+// let a = {'0':'parth','1':'anmol'};
+// // a=[1,2,3];
+// console.log(slice.call(a));
+//
+// // Array.prototype.hasOwnProperty.call(args,'quack');
+// // args.hasOnw('QUACK');
+// //
+// //
+// // a.slice(1);
+// //
+// // slice.call(a,1);
