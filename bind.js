806c7472fd948e9b7d5270d61c18d475400dfd0a
parth <srivastavaparth2016@gmail.com>
2018-01-18 00:43:06 +0530

    added test cases for bind.js

--- bind.js
+++ bind.js
@@ -9,3 +9,12 @@ module.exports = bindExample;
 
 bindExample('hello')('hi','ho','hahahaha');
 console.log('test case 1 for valid inputs',true);
+
+bindExample('')('');
+console.log('test case 2 for empty params and namespace',true);
+
+bindExample('')('hi','hello');
+console.log('test case 3 for empty namespace',true);
+
+bindExample('hello')('');
+console.log('test case 4 for empty params', true);
